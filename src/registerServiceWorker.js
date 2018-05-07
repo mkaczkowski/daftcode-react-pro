421000b8f3da3a98db94008ed8ce85df24d577ac
mkaczkowski <mkaczkowski@icloud.com>
2018-05-07 22:55:55 +0200

    sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -26,12 +26,14 @@ export default function register() {
     console.info('publicUrl.origin:' + publicUrl.origin);
     console.info('window.location.origin:' + window.location.origin);
     if (publicUrl.origin !== window.location.origin) {
+      console.info('NOPE');
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
+    console.info('OK');
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
