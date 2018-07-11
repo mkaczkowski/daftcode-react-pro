364f9651139b9132cdd6569b715a07cb9ea34214
Mariusz Kaczkowski <mariusz.kaczkowski@dcx.bz>
2018-07-11 14:08:07 +0200

    jest unit test configuration

--- scripts/config/jest/babelTransform.js
+++ scripts/config/jest/babelTransform.js
@@ -3,5 +3,10 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app')],
+  presets: [
+    require.resolve("babel-preset-env"),
+    require.resolve("babel-preset-flow"),
+    require.resolve("babel-preset-react"),
+    require.resolve("babel-preset-stage-1"),
+  ],
 });
